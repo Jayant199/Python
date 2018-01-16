19fbb0f868ba4639e43595939669144f1e3a3ca8
Jayant199 <35484107+Jayant199@users.noreply.github.com>
2018-01-16 15:02:34 +0530

    Add files via upload

---
+++ progarm3.py
@@ -0,0 +1,34 @@
+#Mathematical operations 
+
+
+# Subtraction of numbers
+
+a=77
+b=44
+
+print("Subtraction of" ,a,"and",b,"is",a-b)
+
+# division of numbers
+
+a=22
+b=88
+
+print("Division of" ,a,"by",b,"is",a/b)
+
+
+#Multiplication of numbers
+
+a=99
+b=77
+
+
+print("Multiplication of" ,a,"by",b,"is",a*b)
+
+#Modulus operator
+
+a=99
+b=7
+
+
+print("modulus" ,a,"and",b,"is",a%b)
+
---
+++ program2.py
@@ -0,0 +1,15 @@
+# Adding 2 numbers
+
+
+a=103
+b=206
+c=a+b
+print ("Addition of",a,"and",b,"is",c)
+
+
+# Without third variable
+
+a=10
+b=67
+
+print (a+b)
