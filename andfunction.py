77ff88d8bbbcf90f1f686ba22e8a00df097962c5
Jayant199 <35484107+Jayant199@users.noreply.github.com>
2018-01-30 15:14:06 +0530

    Add files via upload

---
+++ andfunction.py
@@ -0,0 +1,17 @@
+# AND function
+
+# To check if a person is eligible to write the exam
+
+a=int(input("Enter your age"))
+
+if (a>=18 and a<= 24):
+    print ("You are Eligible to write the exam")
+
+else:
+    print("You are not eligible to write the exam")
+
+
+
+# OR function
+
+# To check if
